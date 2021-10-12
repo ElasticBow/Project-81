2a9ebfbcf48115b82cb3dfdee30bc31b09187611
ElasticBow <79611589+ElasticBow@users.noreply.github.com>
2021-10-11 17:42:24 -0700

    Add files via upload

--- main.js
+++ main.js
@@ -1,38 +1,38 @@
-canvas=document.getElementById("mycanvas");
+canvas = document.getElementById("myCanvas");
 ctx=canvas.getContext("2d");
 
 ctx.beginPath();
-ctx.strokeStyle="grey";
-ctx.lineWidth=4;
+ctx.strokeStyle = "Grey";
+ctx.lineWidth = 4;
 ctx.rect(150, 143, 430, 200);
 ctx.stroke();
 
 ctx.beginPath();
-ctx.strokeStyle = "blue";
+ctx.strokeStyle = "Blue";
 ctx.lineWidth = 5;
-ctx.arc(250, 40 ,0 , 2 * Math.PI);
+ctx.arc(250, 210, 40 ,0 , 2 * Math.PI);
 ctx.stroke();
 
 ctx.beginPath();
-ctx.strokeStyle = "black";
+ctx.strokeStyle = "Black";
 ctx.lineWidth = 5;
-ctx.arc(350, 40 ,0 , 2 * Math.PI);
+ctx.arc(350, 210, 40 ,0 , 2 * Math.PI);
 ctx.stroke();
 
 ctx.beginPath();
-ctx.strokeStyle = "red";
+ctx.strokeStyle = "Red";
 ctx.lineWidth = 5;
-ctx.arc(450, 40 ,0 , 2 * Math.PI);
+ctx.arc(450, 210, 40 ,0 , 2 * Math.PI);
 ctx.stroke();
 
 ctx.beginPath();
-ctx.strokeStyle = "yellow";
+ctx.strokeStyle = "Yellow";
 ctx.lineWidth = 5;
-ctx.arc(300, 80 ,0 , 2 * Math.PI);
+ctx.arc(300, 250, 40 ,0 , 2 * Math.PI);
 ctx.stroke();
 
 ctx.beginPath();
-ctx.strokeStyle = "green";
+ctx.strokeStyle = "Green";
 ctx.lineWidth = 5;
-ctx.arc(400, 80 ,0 , 2 * Math.PI);
+ctx.arc(400, 250, 40 ,0 , 2 * Math.PI);
 ctx.stroke();
